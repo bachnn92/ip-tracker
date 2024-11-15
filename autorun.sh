992e71c8404105ba39068ce988238d9e2b0e359e
bachnn92 <bachnn92@gmail.com>
2024-11-15 21:16:19 +0700

    update code

--- autorun.sh
+++ autorun.sh
@@ -1,7 +1,7 @@
 cd /home/bachnn92/ip-tracker
 ifconfig > "/home/bachnn92/ip-tracker/logs/$(date).txt"
 git checkout develop
-git reset --hard origin develop
+git reset --hard origin
 git pull -f
 git add .
 git commit -m "Track logs $(date)"
