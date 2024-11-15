b7b974f93eb8b1397ba9314f5dab9156088bd664
bachnn92 <bachnn92@gmail.com>
2024-11-15 21:02:15 +0700

    Track logs Fri Nov 15 21:02:15 +07 2024

--- autorun.sh
+++ autorun.sh
@@ -1,5 +1,5 @@
-cd ~/Helloworld
-df > "~/Helloworld/logs/$(date).txt"
+cd /home/bachnn92/Helloworld
+df > "/home/bachnn92/Helloworld/logs/$(date).txt"
 git add .
 git commit -m "Track logs $(date)"
 git push
