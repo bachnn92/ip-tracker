64d5ce0c70b670eceb969f31a4c2c416c2eed306
bachnn92 <bachnn92@gmail.com>
2024-11-15 21:10:53 +0700

    Track logs Fri Nov 15 21:10:53 +07 2024

--- autorun.sh
+++ autorun.sh
@@ -1,5 +1,5 @@
-cd /home/bachnn92/Helloworld
-ifconfig > "/home/bachnn92/Helloworld/logs/$(date).txt"
+cd /home/bachnn92/ip-tracker
+ifconfig > "/home/bachnn92/ip-tracker/logs/$(date).txt"
 git add .
 git commit -m "Track logs $(date)"
 git push
